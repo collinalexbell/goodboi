62251a87a439a287b9a3fd83a421b18cacfb1b54
kuberlog <collinalexbell@gmail.com>
2021-06-06 17:49:38 -0500

    corner3_with_bottom

--- index.js
+++ index.js
@@ -11,17 +11,31 @@ const slot_dims = {
 function slot(length) {
     return difference(
         cube([length, slot_dims.width, slot_dims.depth]),
-        cube([length+1, plexi_hole_width, 7]).translate([0,0,4.5])
+        cube([length+1, plexi_hole_width, 7]).translate([0,0,5.5])
     );
 }
 
-const corner = union(
+function double_slot(length) {
+    return difference(
+        slot(20),
+        cube([length+1, plexi_hole_width, 7]).translate([0,0,5.5]).rotate([-90, 0 , 0])
+    )
+}
+
+const corner2 = union(
     slot(20).translate([10+plexi_hole_width/2,0,0]),
     slot(20).rotate([0,0,90]).translate([0, 10+plexi_hole_width/2, 0]),
     cube([slot_dims.width/2+plexi_hole_width/2+0.1, slot_dims.width/2+plexi_hole_width/2+0.1, slot_dims.depth]).translate([-plexi_hole_width+0.05, - plexi_hole_width+0.05, 0])
 )
 
+const corner3 = union(
+    double_slot(20).translate([10+plexi_hole_width/2,0,0]),
+    double_slot(20).rotate([90,0,90]).translate([0, 10+plexi_hole_width/2, 0]),
+    cube([slot_dims.width/2+plexi_hole_width/2+0.1, slot_dims.width/2+plexi_hole_width/2+0.1, slot_dims.depth]).translate([-plexi_hole_width+0.05, - plexi_hole_width+0.05, 0]),
+    double_slot(20).rotate([0,90,0]).translate([0,0,20/2 + plexi_hole_width/2])
+)
+
 fs.writeFileSync(
     'scad/cube-test.scad',
-    corner.serialize({ $fn: 20 })
+    corner3.serialize({ $fn: 20 })
 );
